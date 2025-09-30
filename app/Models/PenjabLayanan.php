21ad692ce58da6881725fa00dbcdecb6444c96a8
Djaroephon <djaroephon@gmail.com>
2025-09-30 10:47:25 +0700

    ERD: Make new Relation fo penjab

--- app/Models/PenjabLayanan.php
+++ app/Models/PenjabLayanan.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class PenjabLayanan extends Model
 {
-      protected $table = 'penjab_layanan';
+       protected $table = 'penjab_layanan';
 
     protected $fillable = ['penjab_id', 'nama_penjab_layanan','deskripsi'];
 
@@ -25,31 +25,30 @@ class PenjabLayanan extends Model
         return $this->hasManyThrough(
             Laporan::class,
             Penyelesaian::class,
-            'penjab_layanan_id', // Foreign key on penyelesaian table
-            'id', // Foreign key on laporan table
-            'id', // Local key on penjab_layanan table
-            'laporan_id' // Local key on penyelesaian table
+            'penjab_layanan_id',
+            'id',
+            'id',
+            'laporan_id'
         );
     }
 
-    // Relasi dengan teknisi melalui penyelesaian dan laporan_teknisi
     public function teknisis()
     {
         return $this->hasManyThrough(
             Teknisi::class,
             Penyelesaian::class,
-            'penjab_layanan_id', // Foreign key on penyelesaian table
-            'id', // Foreign key on teknisi table
-            'id', // Local key on penjab_layanan table
-            'laporan_id' // Local key on penyelesaian table
+            'penjab_layanan_id',
+            'id',
+            'id',
+            'laporan_id'
         )->join('laporan_teknisi', 'laporan_teknisi.laporan_id', '=', 'penyelesaian.laporan_id')
          ->whereColumn('laporan_teknisi.teknisi_id', 'teknisi.id')
          ->distinct();
     }
 
-    // Count laporans
     public function getLaporansCountAttribute()
     {
         return $this->laporans()->count();
     }
 }
+
