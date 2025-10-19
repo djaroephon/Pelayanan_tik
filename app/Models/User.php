9061bb5ea6560326640d436b67c53758dd99435c
Djaroephon <djaroephon@gmail.com>
2025-10-19 15:53:06 +0700

    Clean Code & add new Route

--- app/Models/User.php
+++ app/Models/User.php
@@ -53,7 +53,7 @@ class User extends Authenticatable
     }
 
     public function penjabLayanans()
-{
-    return $this->hasMany(PenjabLayanan::class, 'penjab_id');
-}
+    {
+        return $this->hasMany(PenjabLayanan::class, 'penjab_id');
+    }
 }
