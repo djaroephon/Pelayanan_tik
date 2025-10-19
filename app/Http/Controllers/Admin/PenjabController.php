c583ea79abd19ec1d8171c2e271b076325da3f44
Djaroephon <djaroephon@gmail.com>
2025-10-19 15:51:14 +0700

    clean code for penjab

--- app/Http/Controllers/Admin/PenjabController.php
+++ app/Http/Controllers/Admin/PenjabController.php
@@ -3,15 +3,14 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Models\PenjabLayanan;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use App\Models\Laporan;
+use App\Models\PenjabLayanan;
 use App\Models\Teknisi;
+use Illuminate\Support\Facades\Auth;
 
 class PenjabController extends Controller
 {
-// Helper method untuk mendapatkan penjabLayanans
+    // Helper method untuk mendapatkan penjabLayanans
     private function getPenjabLayanans()
     {
         return PenjabLayanan::where('penjab_id', Auth::id())
@@ -30,37 +29,37 @@ class PenjabController extends Controller
                 'teknisis' => collect(),
                 'totalLayanan' => 0,
                 'laporanAktif' => 0,
-                'laporanSelesai' => 0
+                'laporanSelesai' => 0,
             ]);
         }
 
         $layananIds = $penjabLayanans->pluck('id')->toArray();
 
-        $laporans = Laporan::whereHas('penyelesaian', function($query) use ($layananIds) {
+        $laporans = Laporan::whereHas('penyelesaian', function ($query) use ($layananIds) {
             $query->whereIn('penjab_layanan_id', $layananIds);
         })->with(['kategori', 'teknisis', 'penyelesaian.penjabLayanan'])->latest()->take(10)->get();
 
-        $teknisis = Teknisi::whereHas('laporans.penyelesaian', function($query) use ($layananIds) {
+        $teknisis = Teknisi::whereHas('laporans.penyelesaian', function ($query) use ($layananIds) {
             $query->whereIn('penjab_layanan_id', $layananIds);
         })->withCount([
-            'laporans as total_laporans_count' => function($query) use ($layananIds) {
-                $query->whereHas('penyelesaian', function($q) use ($layananIds) {
+            'laporans as total_laporans_count' => function ($query) use ($layananIds) {
+                $query->whereHas('penyelesaian', function ($q) use ($layananIds) {
                     $q->whereIn('penjab_layanan_id', $layananIds);
                 });
             },
-            'laporans as active_laporans_count' => function($query) use ($layananIds) {
-                $query->whereHas('penyelesaian', function($q) use ($layananIds) {
+            'laporans as active_laporans_count' => function ($query) use ($layananIds) {
+                $query->whereHas('penyelesaian', function ($q) use ($layananIds) {
                     $q->whereIn('penjab_layanan_id', $layananIds);
                 })->where('status', 'on progress');
-            }
+            },
         ])->get();
 
         $totalLayanan = $penjabLayanans->count();
-        $laporanAktif = Laporan::whereHas('penyelesaian', function($query) use ($layananIds) {
+        $laporanAktif = Laporan::whereHas('penyelesaian', function ($query) use ($layananIds) {
             $query->whereIn('penjab_layanan_id', $layananIds);
         })->whereIn('status', ['pending', 'on progress'])->count();
 
-        $laporanSelesai = Laporan::whereHas('penyelesaian', function($query) use ($layananIds) {
+        $laporanSelesai = Laporan::whereHas('penyelesaian', function ($query) use ($layananIds) {
             $query->whereIn('penjab_layanan_id', $layananIds);
         })->where('status', 'complete')->count();
 
@@ -79,16 +78,17 @@ class PenjabController extends Controller
         $layanan = PenjabLayanan::where('penjab_id', Auth::id())->findOrFail($id);
         $penjabLayanans = $this->getPenjabLayanans();
 
-        $teknisis = Teknisi::whereHas('laporans.penyelesaian', function($query) use ($id) {
+        $teknisis = Teknisi::whereHas('laporans.penyelesaian', function ($query) use ($id) {
             $query->where('penjab_layanan_id', $id);
-        })->with(['laporans' => function($query) use ($id) {
-            $query->whereHas('penyelesaian', function($q) use ($id) {
+        })->with(['laporans' => function ($query) use ($id) {
+            $query->whereHas('penyelesaian', function ($q) use ($id) {
                 $q->where('penjab_layanan_id', $id);
             });
-        }])->get()->map(function($teknisi) {
+        }])->get()->map(function ($teknisi) {
             $teknisi->active_laporans_count = $teknisi->laporans->where('status', 'on progress')->count();
             $teknisi->total_laporans_count = $teknisi->laporans->count();
             $teknisi->is_active = $teknisi->active_laporans_count > 0;
+
             return $teknisi;
         });
 
@@ -100,7 +100,7 @@ class PenjabController extends Controller
         $layanan = PenjabLayanan::where('penjab_id', Auth::id())->findOrFail($id);
         $penjabLayanans = $this->getPenjabLayanans();
 
-        $laporans = Laporan::whereHas('penyelesaian', function($query) use ($id) {
+        $laporans = Laporan::whereHas('penyelesaian', function ($query) use ($id) {
             $query->where('penjab_layanan_id', $id);
         })->with(['kategori', 'teknisis', 'penyelesaian'])->latest()->paginate(15);
 
@@ -118,7 +118,7 @@ class PenjabController extends Controller
 
         $layananIds = $penjabLayanans->pluck('id')->toArray();
 
-        $laporans = Laporan::whereHas('penyelesaian', function($query) use ($layananIds) {
+        $laporans = Laporan::whereHas('penyelesaian', function ($query) use ($layananIds) {
             $query->whereIn('penjab_layanan_id', $layananIds);
         })->with(['kategori', 'teknisis', 'penyelesaian.penjabLayanan'])->latest()->paginate(15);
 
@@ -136,7 +136,7 @@ class PenjabController extends Controller
 
         $layananIds = $penjabLayanans->pluck('id')->toArray();
 
-        $laporan = Laporan::whereHas('penyelesaian', function($query) use ($layananIds) {
+        $laporan = Laporan::whereHas('penyelesaian', function ($query) use ($layananIds) {
             $query->whereIn('penjab_layanan_id', $layananIds);
         })->with(['kategori', 'teknisis', 'penyelesaian.penjabLayanan'])->findOrFail($id);
 
@@ -148,34 +148,34 @@ class PenjabController extends Controller
         $layanan = PenjabLayanan::where('penjab_id', Auth::id())->findOrFail($id);
         $penjabLayanans = $this->getPenjabLayanans();
 
-        $totalLaporanLayanan = Laporan::whereHas('penyelesaian', function($query) use ($id) {
+        $totalLaporanLayanan = Laporan::whereHas('penyelesaian', function ($query) use ($id) {
             $query->where('penjab_layanan_id', $id);
         })->count();
 
-        $laporanAktifLayanan = Laporan::whereHas('penyelesaian', function($query) use ($id) {
+        $laporanAktifLayanan = Laporan::whereHas('penyelesaian', function ($query) use ($id) {
             $query->where('penjab_layanan_id', $id);
         })->whereIn('status', ['pending', 'on progress'])->count();
 
-        $laporanSelesaiLayanan = Laporan::whereHas('penyelesaian', function($query) use ($id) {
+        $laporanSelesaiLayanan = Laporan::whereHas('penyelesaian', function ($query) use ($id) {
             $query->where('penjab_layanan_id', $id);
         })->where('status', 'complete')->count();
 
-        $teknisisLayanan = Teknisi::whereHas('laporans.penyelesaian', function($query) use ($id) {
+        $teknisisLayanan = Teknisi::whereHas('laporans.penyelesaian', function ($query) use ($id) {
             $query->where('penjab_layanan_id', $id);
         })->withCount([
-            'laporans as total_laporans_count' => function($query) use ($id) {
-                $query->whereHas('penyelesaian', function($q) use ($id) {
+            'laporans as total_laporans_count' => function ($query) use ($id) {
+                $query->whereHas('penyelesaian', function ($q) use ($id) {
                     $q->where('penjab_layanan_id', $id);
                 });
             },
-            'laporans as active_laporans_count' => function($query) use ($id) {
-                $query->whereHas('penyelesaian', function($q) use ($id) {
+            'laporans as active_laporans_count' => function ($query) use ($id) {
+                $query->whereHas('penyelesaian', function ($q) use ($id) {
                     $q->where('penjab_layanan_id', $id);
                 })->where('status', 'on progress');
-            }
+            },
         ])->get();
 
-        $laporansLayanan = Laporan::whereHas('penyelesaian', function($query) use ($id) {
+        $laporansLayanan = Laporan::whereHas('penyelesaian', function ($query) use ($id) {
             $query->where('penjab_layanan_id', $id);
         })->with(['kategori', 'teknisis', 'penyelesaian.penjabLayanan'])->latest()->take(10)->get();
 
@@ -189,7 +189,4 @@ class PenjabController extends Controller
             'laporansLayanan'
         ));
     }
-
-
 }
-
